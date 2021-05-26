0bc8c080853d516064444187c4098ab13dd51dff
Dimon0205 <lepdmitrijj@yandex.ru>
2021-05-26 23:00:08 +0300

    my lesson

--- lesson.py
+++ lesson.py
@@ -1,5 +1,3 @@
-
-
 # required -- required data
 # weight -- train weight
 # axes--  number of axes
@@ -17,37 +15,58 @@ from decimal import Decimal, getcontext
 
 getcontext().prec = 5
 
+out_result = {}
 axes = int(input('Количество осей: ') or '0')
-press_coef = [0.33, 0.55]
+press_coef = [0.55, 0.33, 0.32, 0.31, 0.30]
 weight = float(input('Вес поезда: ') or '0')
 parking_axes = weight * 0.006
 
 press_axes_list = {
-    3.5: 0.0,
-    4.5: 0.0,
-    7.0: 0.0,
-    7.5: 0.0,
-    8.0: 0.0,
-    8.5: 0.0
+    3.5: 0,
+    4.5: 0,
+    7.0: 0,
+    7.5: 0,
+    8.0: 0,
+    8.5: 0
 }
 
 press_axes = {}
 for k, v in press_axes_list.items():
-    v = float(input(k) or '0')
+    v = int(input(k) or '0')
     if v > 0:
-        s = v * k
-        print(k, v, s)
+        p = float(v) * k
+        p = int(p)
+        print(k, v, p)
         if v != 0:
-            for i in range(6):
-                i = i + 1
-                r = [k, v, s]
-                press_axes = r
+            press_axes[k] = [v, p]
+total_press_axes = [sum(i) for i in zip(*press_axes.values())]
 
-print(press_axes)
+# print(press_axes)
+# print('Итого: ' + str(total_press_axes))
+pressresult = 0
 press_axes_list = list(press_axes_list)
-if press_axes_list[2::] == 0:
-    pressresult = Decimal(weight) * Decimal(press_coef[1])
-else:
-    pressresult = Decimal(weight) * Decimal(press_coef[0])
-print('Вес поезда: ' + str(int(weight)), 'Требуемое тормозное нажатие: ' + str(math.ceil(pressresult)))
-print('Требуется ручных тормозов: ' + str(math.ceil(parking_axes)), 'Всего осей: ' + str(axes), end='\n')
+for total in total_press_axes:
+    if press_axes_list[2::] == 0:
+        pressresult = Decimal(weight) * Decimal(press_coef[0])
+    if press_axes_list[2::] != 0:
+        if pressresult == 0:
+            pressresult = [Decimal(weight) * Decimal(press_coef[1])]
+        # if pressresult > total:
+        #     pressresult = [Decimal(weight) * Decimal(press_coef[2])]
+        # if pressresult > total:
+        #     pressresult = [Decimal(weight) * Decimal(press_coef[3])]
+        # if pressresult > total:
+        #     pressresult = [Decimal(weight) * Decimal(press_coef[4])]
+
+# pressresult = int(pressresult)
+
+out_result.update({'Вес поезда: ': int(weight),
+                   'Количество осей: ': axes,
+                   'Требуемое нажатие: ': pressresult,
+                   'Требуется ручных тормозных осей:': parking_axes,
+                   press_axes.keys(): press_axes.values(),
+                   'Итого: ': total_press_axes
+                   })
+print(out_result)
+# print('Вес поезда: ' + str(int(weight)), 'Требуемое тормозное нажатие: ' + str(math.ceil(pressresult)))
+# print('Требуется ручных тормозов: ' + str(math.ceil(parking_axes)), 'Всего осей: ' + str(axes))
---
+++ my.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GrdLayout 
+
+class Calculator(App)
+	def build(self)
+		g1 = GridLayout(cols = 1)
+
+		
